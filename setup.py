8da3835bd5e903d2a732a445c95814dd50f4630f
Jehiah Czebotar <jehiah@gmail.com>
2010-07-14 23:20:15 -0400

    add long description

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
 from distutils.core import setup
 
 setup(name='urlnorm',
-      version='1.0',
-      description="Normalize a URL to a standard unicode encoding",
-      py_modules=['urlnorm'],
-      license='MIT License',
-      author='Jehiah Czebotar',
-      author_email='"Jehiah Czebotar" <jehiah@gmail.com>',
-      url='http://github.com/jehiah/urlnorm',
-      )
+        version='1.0',
+        long_description=open("./README.txt", "r").read(),
+        description="Normalize a URL to a standard unicode encoding",
+        py_modules=['urlnorm'],
+        license='MIT License',
+        author='Jehiah Czebotar',
+        author_email='"Jehiah Czebotar" <jehiah@gmail.com>',
+        url='http://github.com/jehiah/urlnorm',
+        )
