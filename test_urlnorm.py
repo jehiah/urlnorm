c038f6c31a246fd302f2b753630592c8f1c83bab
Jehiah Czebotar <jehiah@gmail.com>
2010-12-30 13:45:56 -0500

    int2ip, collate %20 -> +, parameter handling

--- test_urlnorm.py
+++ test_urlnorm.py
@@ -2,12 +2,70 @@
 this is a py.test test file
 """
 import urlnorm
+from urlnorm import _unicode
 
 def pytest_generate_tests(metafunc):
     if metafunc.function in [test_norms]:
         """ test suite; some taken from RFC1808. Run with py.test"""
-        tests = {    
-            '/foo/bar/.':                    '/foo/bar/', 
+        tests = {
+            'http://1113982867/':            'http://66.102.7.147/', # ip dword encoding
+            'http://www.foo.com:80/foo':     'http://www.foo.com/foo',
+            'http://www.foo.com:8000/foo':   'http://www.foo.com:8000/foo',
+            'http://www.foo.com./foo/bar.html': 'http://www.foo.com/foo/bar.html',
+            'http://www.foo.com.:81/foo':    'http://www.foo.com:81/foo',
+            'http://www.foo.com/%7ebar':     'http://www.foo.com/~bar',
+            'http://www.foo.com/%7Ebar':     'http://www.foo.com/~bar',
+            'ftp://user:pass@ftp.foo.net/foo/bar': 'ftp://user:pass@ftp.foo.net/foo/bar',
+            'http://USER:pass@www.Example.COM/foo/bar': 'http://USER:pass@www.example.com/foo/bar',
+            'http://www.example.com./':      'http://www.example.com/',
+            'http://test.example/?a=%26&b=1': 'http://test.example/?a=%26&b=1', # should not un-encode the & that is part of a parameter value
+            'http://test.example/?a=%e3%82%82%26': 'http://test.example/?a=\xe3\x82\x82%26'.decode('utf8'), # should return a unicode character
+            # note: this breaks the internet for parameters that are positional (stupid nextel) and/or don't have an = sign
+            # 'http://test.example/?a=1&b=2&a=3': 'http://test.example/?a=1&a=3&b=2', # should be in sorted/grouped order
+            
+            # 'http://s.xn--q-bga.de/':       'http://s.q\xc3\xa9.de/'.decode('utf8'), # should be in idna format
+            'http://test.example/?':        'http://test.example/', # no trailing ?
+            'http://test.example?':       'http://test.example/', # with trailing /
+            'http://a.COM/path/?b&a' : 'http://a.com/path/?b&a',
+            # test utf8 and unicode
+            u'http://XBLA\u306eXbox.com': 'http://xbla\xe3\x81\xaexbox.com/'.decode('utf8'),
+            u'http://XBLA\u306eXbox.com'.encode('utf8'): 'http://xbla\xe3\x81\xaexbox.com/'.decode('utf8'),
+            u'http://XBLA\u306eXbox.com': 'http://xbla\xe3\x81\xaexbox.com/'.decode('utf8'),
+            # test idna + utf8 domain
+            # u'http://xn--q-bga.XBLA\u306eXbox.com'.encode('utf8'): 'http://q\xc3\xa9.xbla\xe3\x81\xaexbox.com'.decode('utf8'),
+            'http://ja.wikipedia.org/wiki/%E3%82%AD%E3%83%A3%E3%82%BF%E3%83%94%E3%83%A9%E3%83%BC%E3%82%B8%E3%83%A3%E3%83%91%E3%83%B3': 'http://ja.wikipedia.org/wiki/\xe3\x82\xad\xe3\x83\xa3\xe3\x82\xbf\xe3\x83\x94\xe3\x83\xa9\xe3\x83\xbc\xe3\x82\xb8\xe3\x83\xa3\xe3\x83\x91\xe3\x83\xb3'.decode('utf8'),
+            'http://test.example/\xe3\x82\xad': 'http://test.example/\xe3\x82\xad',
+            
+            # check that %23 (#) is not escaped where it shouldn't be
+            'http://test.example/?p=%23val#test-%23-val%25': 'http://test.example/?p=%23val#test-%23-val%25',
+            # check that %20 or %25 is not unescaped to ' ' or %
+            'http://test.example/%25/?p=%20val%20%25' : 'http://test.example/%25/?p=+val+%25',
+            "http://test.domain/I%C3%B1t%C3%ABrn%C3%A2ti%C3%B4n%EF%BF%BDliz%C3%A6ti%C3%B8n" : "http://test.domain/I\xc3\xb1t\xc3\xabrn\xc3\xa2ti\xc3\xb4n\xef\xbf\xbdliz\xc3\xa6ti\xc3\xb8n",
+            # check that spaces are collated to '+'
+            "http://test.example/path/with a%20space/" : "http://test.example/path/with+a+space/",
+        }
+        for bad, good in tests.items():
+            metafunc.addcall(funcargs=dict(bad=bad, good=good))
+    
+    elif metafunc.function == test_unquote:
+        for bad, good, unsafe in (
+            ('%20', ' ', ''),
+            ('%3f', '%3F', '?'), # don't unquote it, but uppercase it
+            ('%E3%82%AD', u'\u30ad', ''),
+            ):
+            metafunc.addcall(funcargs=dict(bad=bad, good=good, unsafe=unsafe))
+    
+    elif metafunc.function in [test_invalid_urls]:
+        for url in [
+            'http://http://www.exemple.com/', # invalid domain
+            '-',
+            'asdf',
+            'HTTP://4294967297/test', # one more than max ip > int
+            ]:
+            metafunc.addcall(funcargs=dict(url=url))
+    elif metafunc.function == test_norm_path:
+        tests = {
+            '/foo/bar/.':                    '/foo/bar/',
             '/foo/bar/./':                   '/foo/bar/',
             '/foo/bar/..':                   '/foo/',
             '/foo/bar/../':                  '/foo/',
@@ -28,59 +86,27 @@ def pytest_generate_tests(metafunc):
             '/foo/./bar':                    '/foo/bar',
             '/foo/../bar':                   '/bar',
             '/foo//':                        '/foo/',
-            '/foo///bar//':                  '/foo/bar/',    
-            'http://www.foo.com:80/foo':     'http://www.foo.com/foo',
-            'http://www.foo.com:8000/foo':   'http://www.foo.com:8000/foo',
-            'http://www.foo.com./foo/bar.html': 'http://www.foo.com/foo/bar.html',
-            'http://www.foo.com.:81/foo':    'http://www.foo.com:81/foo',
-            'http://www.foo.com/%7ebar':     'http://www.foo.com/~bar',
-            'http://www.foo.com/%7Ebar':     'http://www.foo.com/~bar',
-            'ftp://user:pass@ftp.foo.net/foo/bar': 'ftp://user:pass@ftp.foo.net/foo/bar',
-            'http://USER:pass@www.Example.COM/foo/bar': 'http://USER:pass@www.example.com/foo/bar',
-            'http://www.example.com./':      'http://www.example.com/',
-            'http://test.example/?a=%26&b=1': 'http://test.example/?a=%26&b=1', # should not un-encode the & that is part of a parameter value
-            'http://test.example/?a=%e3%82%82%26': 'http://test.example/?a=\xe3\x82\x82%26'.decode('utf8'), # should return a unicode character
-            # note: this breaks the internet for parameters that are positional (stupid nextel) and/or don't have an = sign
-            # 'http://test.example/?a=1&b=2&a=3': 'http://test.example/?a=1&a=3&b=2', # should be in sorted/grouped order
-            'http://s.xn--q-bga.de/':       'http://s.q\xc3\xa9.de/'.decode('utf8'), # should be in idna format
-            'http://test.example/?':        'http://test.example/', # no trailing ?
-            # 'http://test.example?':       'http://test.example/', # with trailing /
-            'http://a/path/?b&a' : 'http://a/path/?b&a',
-            # test utf8 and unicode
-            u'http://XBLA\u306eXbox.com': 'http://xbla\xe3\x81\xaexbox.com'.decode('utf8'),
-            u'http://XBLA\u306eXbox.com'.encode('utf8'): 'http://xbla\xe3\x81\xaexbox.com'.decode('utf8'),
-            u'http://XBLA\u306eXbox.com': 'http://xbla\xe3\x81\xaexbox.com'.decode('utf8'),
-            # test idna + utf8 domain
-            u'http://xn--q-bga.XBLA\u306eXbox.com'.encode('utf8'): 'http://q\xc3\xa9.xbla\xe3\x81\xaexbox.com'.decode('utf8'),
-            '-':                             '-',
-            'http://ja.wikipedia.org/wiki/%E3%82%AD%E3%83%A3%E3%82%BF%E3%83%94%E3%83%A9%E3%83%BC%E3%82%B8%E3%83%A3%E3%83%91%E3%83%B3': 'http://ja.wikipedia.org/wiki/\xe3\x82\xad\xe3\x83\xa3\xe3\x82\xbf\xe3\x83\x94\xe3\x83\xa9\xe3\x83\xbc\xe3\x82\xb8\xe3\x83\xa3\xe3\x83\x91\xe3\x83\xb3'.decode('utf8'),
-            
-            # check that %23 (#) is not escaped where it shouldn't be
-            'http://test.example/?p=%23val#test-%23-val%25': 'http://test.example/?p=%23val#test-%23-val%25',
-            # check that %20 or %25 is not unescaped to ' ' or %
-            'http://test.example/%25/?p=%20val%20%25' : 'http://test.example/%25/?p=%20val%20%25',
+            '/foo///bar//':                  '/foo/bar/',
         }
         for bad, good in tests.items():
             metafunc.addcall(funcargs=dict(bad=bad, good=good))
 
-    elif metafunc.function in [test_invalid_urls]:
-        for url in [
-            'http://http://www.exemple.com/',
-            ]:
-            metafunc.addcall(funcargs=dict(url=url))
-
 def test_invalid_urls(url):
     try:
-        urlnorm.norm(url)
+        output = urlnorm.norm(url)
+        print '%r' % output
     except urlnorm.InvalidUrl:
         return
     assert 1 == 0, "this should have raised an InvalidUrl exception"
 
+def test_unquote(bad, good, unsafe):
+    output = urlnorm.unquote_safe(bad, unsafe)
+    assert output == good
+
 def test_norms(bad, good):
     new_url = urlnorm.norm(bad)
-    print "bad", repr(bad)
-    print "new_url", repr(new_url)
-    print "good", repr(good)
-    print "new_url", new_url
-    print "good", good
-    assert new_url == good
+    assert new_url == _unicode(good)
+
+def test_norm_path(bad, good):
+    output = urlnorm.norm_path("http", bad)
+    assert output == _unicode(good)
--- urlnorm.py
+++ urlnorm.py
@@ -2,28 +2,34 @@
 # -*- coding: utf8 -*-
 
 """
-urlnorm.py - URL normalisation routines
+urlnorm.py - URL normalization routines
 
 urlnorm normalizes a URL by:
   * lowercasing the scheme and hostname
   * converting the hostname to IDN format
   * taking out default port if present (e.g., http://www.foo.com:80/)
-  * collapsing the path (./, ../, etc)
+  * collapsing the path (./, ../, //, etc)
   * removing the last character in the hostname if it is '.'
-  * unquoting any % escaped characters (where possible)
+  * unescaping any percent escape sequences (where possible)
+  * upercase percent escape (ie: %3f => %#F)
+  * converts spaces, and %20 to '+'
+  * converts ip encoded as an integer to dotted quad notation 
 
 Available functions:
-  norm - given a URL (string), returns a normalised URL
-
+  norm - given a URL (string), returns a normalized URL
   norm_netloc
   norm_path
   unquote_path
+  unquote_params
   unquote_qs
   unquote_fragment
-  
- 
+
+
 CHANGES:
-1.0.1 fix problem unescaping %23 and %20 in query string
+1.1 - collate %20 and ' ' to '+'
+1.1 - fix unescaping of parameters
+1.1 - added int2ip
+1.0.1 - fix problem unescaping %23 and %20 in query string
 1.0 - new release
 0.94 - idna handling, unescaping querystring, fragment, add ws + wss ports
 0.92 - unknown schemes now pass the port through silently
@@ -56,7 +62,7 @@ SOFTWARE.
 """
 
 # also update in setup.py
-__version__ = "1.0.1"
+__version__ = "1.1"
 
 from urlparse import urlparse, urlunparse
 from string import lower
@@ -94,17 +100,9 @@ _relative_schemes = [   'http',
                         'file',
                         ''
                     ]
-_server_authority_schemes = [   'http',
-                                'https',
-                                'ws',
-                                'wss',
-                                'itms',
-                                'news',
-                                'snews',
-                                'ftp',
-                            ]
 
-qs_unsafe_list = ' /?&=;+%#'
+params_unsafe_list = ' ?=+%#;'
+qs_unsafe_list = ' ?&=+%#'
 fragment_unsafe_list = ' +%#'
 path_unsafe_list = ' /?;%+#'
 _hextochr = dict(('%02x' % i, chr(i)) for i in range(256))
@@ -113,15 +111,17 @@ _hextochr.update(('%02X' % i, chr(i)) for i in range(256))
 def unquote_path(s):
     return unquote_safe(s, path_unsafe_list)
 
+def unquote_params(s):
+    return unquote_safe(s, params_unsafe_list)
+
 def unquote_qs(s):
     return unquote_safe(s, qs_unsafe_list)
 
 def unquote_fragment(s):
-    """unquote('%')"""
     return unquote_safe(s, fragment_unsafe_list)
-    
+
 def unquote_safe(s, unsafe_list):
-    """unquote('abc%20def') -> 'abc def'."""
+    """unquote percent escaped string except for percent escape sequences that are in unsafe_list"""
     # note: this build utf8 raw strings ,then does a .decode('utf8') at the end.
     # as a result it's doing .encode('utf8') on each block of the string as it's processed.
     res = _utf8(s).split('%')
@@ -130,7 +130,8 @@ def unquote_safe(s, unsafe_list):
         try:
             raw_chr = _hextochr[item[:2]]
             if raw_chr in unsafe_list or ord(raw_chr) < 20:
-                res[i] = '%' + item
+                # leave it unescaped (but uppercase the percent escape)
+                res[i] = '%' + item[:2].upper() + item[2:]
             else:
                 res[i] = raw_chr + item[2:]
         except KeyError:
@@ -141,26 +142,26 @@ def unquote_safe(s, unsafe_list):
     o = "".join(res)
     return _unicode(o)
 
-always_safe = ('ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-               'abcdefghijklmnopqrstuvwxyz'
-               '0123456789' '_.-')
-_safemaps = {}
-
 def norm(url):
-    """given a string URL, return its normalised/unicode form"""
+    """given a string URL, return its normalized/unicode form"""
     url = _unicode(url) # operate on unicode strings
     url_tuple = urlparse(url)
     normalized_tuple = norm_tuple(*url_tuple)
-    return urlunparse(normalized_tuple)
+    return urlunparse(normalized_tuple).replace(' ','+').replace('%20','+')
 
 def norm_tuple(scheme, authority, path, parameters, query, fragment):
-    """given individual url components, return its normalised form"""
+    """given individual url components, return its normalized form"""
     scheme = lower(scheme)
+    if not scheme:
+        raise InvalidUrl('missing URL scheme')
     authority = norm_netloc(scheme, authority)
+    if not authority:
+        raise InvalidUrl('missing netloc')
     path = norm_path(scheme, path)
     # TODO: put query in sorted order; or at least group parameters together
     # Note that some websites use positional parameters or the name part of a query so this would break the internet
     # query = urlencode(parse_qs(query, keep_blank_values=1), doseq=1) 
+    parameters = unquote_params(parameters)
     query = unquote_qs(query)
     fragment = unquote_fragment(fragment)
     return (scheme, authority, path, parameters, query, fragment)
@@ -174,8 +175,21 @@ def norm_path(scheme, path):
                 break
             last_path = path
     path = unquote_path(path)
+    if not path:
+        return '/'
     return path
 
+MAX_IP=0xffffffffL
+def int2ip(ipnum):
+    assert isinstance(ipnum, int)
+    if MAX_IP < ipnum or ipnum < 0:
+        raise TypeError("expected int between 0 and %d inclusive" % MAX_IP)
+    ip1 = ipnum >> 24
+    ip2 = ipnum >> 16 & 0xFF
+    ip3 = ipnum >> 8 & 0xFF
+    ip4 = ipnum & 0xFF
+    return "%d.%d.%d.%d" % (ip1, ip2, ip3, ip4)
+    
 def norm_netloc(scheme, netloc):
     if not netloc:
         return netloc
@@ -184,26 +198,33 @@ def norm_netloc(scheme, netloc):
         raise InvalidUrl('no host in netloc %r' % netloc)
     
     userinfo, host, port = match.groups()
+    if host.isdigit():
+        try:
+            host = int2ip(int(host))
+        except TypeError:
+            raise InvalidUrl('host %r does not escape to a valid ip' % host)
     if host[-1] == '.':
         host = host[:-1]
     
     authority = lower(host)
     if 'xn--' in authority:
-        # convert each section of the domain from idna individually
-        domains = authority.split('.')
-        for i in range(len(domains)):
-            if 'xn--' in domains[i]:
-                try:
-                    domains[i] = domains[i].decode('idna')
-                except UnicodeError:
-                    raise InvalidUrl('Error converting domain to IDN %r' % authority)
-        authority = '.'.join(domains)
+        subdomains = [_idn(subdomain) for subdomain in authority.split('.')]
+        authority = '.'.join(subdomains)
         
     if userinfo:
         authority = "%s@%s" % (userinfo, authority)
     if port and port != _default_port.get(scheme, None):
         authority = "%s:%s" % (authority, port)
     return authority
+
+
+def _idn(subdomain):
+    if subdomain.startswith('xn--'):
+        try:
+            subdomain = subdomain.decode('idna')
+        except UnicodeError:
+            raise InvalidUrl('Error converting subdomain %r to IDN' % subdomain)
+    return subdomain
 
 
 def _utf8(value):
